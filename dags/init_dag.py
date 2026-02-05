21ca2e0eef0ed3e7010cb66c005a7ec06411d9cd
Tong Guan <guantong0517@gmail.com>
2026-02-05 13:05:29 -0500

    add snowflake_conn_id to init dag tasks

--- dags/init_dag.py
+++ dags/init_dag.py
@@ -11,46 +11,55 @@ with DAG(
     
     create_schemas = SnowflakeOperator(
         task_id = "create_schemas",
+        snowflake_conn_id="jan_airflow_snowflake",
         sql = "sql/init/00_create_schemas.sql"
     )
 
     create_metadata = SnowflakeOperator(
         task_id="create_metadata",
+        snowflake_conn_id="jan_airflow_snowflake",
         sql="sql/metadata/create_metadata_table_4.sql"
     )
 
     init_metadata = SnowflakeOperator(
         task_id="init_metadata",
+        snowflake_conn_id="jan_airflow_snowflake",
         sql="sql/metadata/init_metadata_record_4.sql"
     )
 
     refresh_staging = SnowflakeOperator(
         task_id="refresh_staging",
+        snowflake_conn_id="jan_airflow_snowflake",
         sql="sql/staging/refresh_staging.sql"
     )
 
     dim_date = SnowflakeOperator(
         task_id="dim_date",
+        snowflake_conn_id="jan_airflow_snowflake",
         sql="sql/init/create_dim_date_4.sql"
     )
 
     dim_company_core = SnowflakeOperator(
         task_id="dim_company_core",
+        snowflake_conn_id="jan_airflow_snowflake",
         sql="sql/init/create_dim_company_core_4.sql"
     )
 
     dim_company_financial = SnowflakeOperator(
         task_id="dim_company_financial",
+        snowflake_conn_id="jan_airflow_snowflake",
         sql="sql/init/create_dim_company_financial_4.sql"
     )
 
     fact = SnowflakeOperator(
         task_id="fact",
+        snowflake_conn_id="jan_airflow_snowflake",
         sql="sql/init/create_fact_stock_price_4.sql"
     )
 
     update_watermark = SnowflakeOperator(
         task_id="update_watermark",
+        snowflake_conn_id="jan_airflow_snowflake",
         sql="sql/metadata/update_watermark_4.sql"
     )
 
