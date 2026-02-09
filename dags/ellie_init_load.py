133642d86a9910d1d8429af7caecd82d6c29349e
Ellie0305 <82900288+Yvonne0305@users.noreply.github.com>
2026-02-08 16:58:21 -0800

    Refactor DAG definition in ellie_init_load.py

--- dags/ellie_init_load.py
+++ dags/ellie_init_load.py
@@ -3,16 +3,14 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 import ellie_01_load_dim_date
 
-ellie_01_load_dim_date.ellie_load_dim_date()
-
 with DAG(
     dag_id="ellie_init_load",
     start_date=datetime(2026, 2, 8),
-    schedule_interval="@daily",  # daily run
-    catchup=False
+    schedule_interval="@daily",
+    catchup=False,
 ) as dag:
 
     task1 = PythonOperator(
         task_id="ellie_01_load_dim_date",
-        python_callable=ellie_load_dim_date
+        python_callable=ellie_01_load_dim_date.ellie_load_dim_date,
     )
